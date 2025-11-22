c727df936dc907fb20de947750b1453d1d7a3c6f
zhiyue <zhiyue@agibang.ai>
2025-11-22 20:53:35 +0000

    Add wav support

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
       },
       base: '/',
       plugins: [react()],
+      assetsInclude: ['**/*.wav'],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
