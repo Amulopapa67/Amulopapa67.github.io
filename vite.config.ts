be653355a01e66dc7f2f2e67245050e6b8e98d87
zhiyue <zhiyue@agibang.ai>
2025-11-22 20:47:34 +0000

    Add WorkFlow

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      base: '/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
